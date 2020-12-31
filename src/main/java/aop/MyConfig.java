bd69d5d8ae6d26d1d8c9d6a1db43f7e5af7e9d7c
Anton <zantn22@gmail.com>
2020-12-31 18:42:25 +0300

    Use Join Point into Advice

--- src/main/java/aop/MyConfig.java
+++ src/main/java/aop/MyConfig.java
@@ -8,4 +8,5 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @ComponentScan("aop")
 @EnableAspectJAutoProxy
 public class MyConfig {
+
 }
--- src/main/java/aop/aspects/LoggingAspect.java
+++ src/main/java/aop/aspects/LoggingAspect.java
@@ -1,8 +1,10 @@
 package aop.aspects;
 
+import aop.Book;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +12,34 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Order(1)
 public class LoggingAspect {
+    @Before("aop.aspects.MyPointcuts.allAddMethods()")
+    public void beforeAddLoggingAdvice(JoinPoint joinPoint) {
 
-//    @Pointcut("execution(* aop.UniLibrary.get*())")
-//        private void allGetMethod() {
-//    }
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("methodSignature + : " + methodSignature);
+        System.out.println("methodSignature.getMethod + : " + methodSignature.getMethod());
+        System.out.println("methodSignature.getReturnType + : " + methodSignature.getReturnType());
+        System.out.println("methodSignature.getName+ : " + methodSignature.getName());
+
+        if(methodSignature.getName().equals("addBook")) {
+            Object[] argument = joinPoint.getArgs();
+                for(Object obj: argument) {
+                    if(obj instanceof Book) {
+                        Book myBook = (Book) obj;
+                        System.out.println("Информация о книге: название - " + myBook.getName()
+                                + "автор - " + myBook.getAuthor() + "год издания - " + myBook.getYearOfPublication());
+                    }
+
+                    else if(obj instanceof String) {
+                        System.out.println("книгу в библиотеку добавляет: " + obj);
+                    }
+                }
+        }
+
+
+        System.out.println("beforeAddLoggingAdvice: логирование попытки получить книгу/журнал");
+        System.out.println("-----------------------------");
+    }
 
 //    @Pointcut("execution(* aop.UniLibrary.return*())")
 //    private void allReturnMethodFromUniLibrary() {
@@ -40,10 +66,7 @@ public class LoggingAspect {
 //
 
 
-    @Before("aop.aspects.MyPointcuts.allGetMethods()")
-    public void beforeGetLoggingAdvice() {
-        System.out.println("beforeGetBookAdvice: логирование попытки получить книгу/журнал");
-    }
+
 
 
 
