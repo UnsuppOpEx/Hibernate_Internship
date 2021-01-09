6ce63d48c60537fc1509ca4701594ca8261eb268
Anton <zantn22@gmail.com>
2021-01-09 23:18:11 +0300

    Connection One-To-One, @OneToOne(CascadeType), @JoinColumn

---
+++ src/main/java/aop/hibernate_test2/Test1.java
@@ -0,0 +1,65 @@
+package aop.hibernate_test2;
+
+import aop.hibernate_test2.entity.Detail;
+import aop.hibernate_test2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+
+        Session session = null;
+        try {
+//            Session session = factory.getCurrentSession();
+//            Employee emp = new Employee("Anton", "Zemskov", "IT", 500);
+//            Detail detail = new Detail("Ivanovo", "123456789", "zemskov@gmail.com");
+//            emp.setEmpDetail(detail);
+//
+//            session.beginTransaction();
+//            session.save(emp);
+//            session.getTransaction().commit();
+//
+//            System.out.println("done!");
+
+//            Session session = factory.getCurrentSession();
+//            Employee emp = new Employee("Oleg", "Smirnov", "Sales", 700);
+//            Detail detail = new Detail("Moskow", "987654321", "olejka@gmail.com");
+//            emp.setEmpDetail(detail);
+//
+//            session.beginTransaction();
+//            session.save(emp);
+//            session.getTransaction().commit();
+//
+//            System.out.println("done!");
+
+//            session = factory.getCurrentSession();
+//
+//            session.beginTransaction();
+//            Employee emp = session.get(Employee.class, 1);
+//            System.out.println(emp.getEmpDetail());
+//            session.getTransaction().commit();
+//
+//            System.out.println("done!");
+
+            session = factory.getCurrentSession();
+
+            session.beginTransaction();
+            Employee emp = session.get(Employee.class, 2);
+            session.delete(emp);
+
+            session.getTransaction().commit();
+            System.out.println("done!");
+        }
+
+        finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
---
+++ src/main/java/aop/hibernate_test2/entity/Employee.java
@@ -0,0 +1,98 @@
+package aop.hibernate_test2.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String firstname;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private int salary;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "details_id")
+    private Detail empDetail;
+
+    public Employee() {
+    }
+
+    public Employee(String firstname, String surname, String department, int salary) {
+        this.firstname = firstname;
+        this.surname = surname;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Detail getEmpDetail() {
+        return empDetail;
+    }
+
+    public void setEmpDetail(Detail empDetail) {
+        this.empDetail = empDetail;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", surname='" + surname + '\'' +
+                ", department='" + department + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
