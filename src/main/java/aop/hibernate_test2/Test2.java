8db56545818ca6e5ae1ba1152b7dfd817f489055
Anton <zantn22@gmail.com>
2021-01-10 00:41:49 +0300

    Connection One-To-One(Uni-directional and Bi-directional)

---
+++ src/main/java/aop/hibernate_test2/Test2.java
@@ -0,0 +1,48 @@
+package aop.hibernate_test2;
+
+import aop.hibernate_test2.entity.Detail;
+import aop.hibernate_test2.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test2 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class)
+                .addAnnotatedClass(Detail.class)
+                .buildSessionFactory();
+
+        Session session = null;
+
+        try {
+//            session = factory.getCurrentSession();
+//            Employee emp = new Employee("Nikolay", "Ivanov", "HR", 850);
+//            Detail detail = new Detail("New-York", "3668954402", "nikolay@gmail.com");
+//            emp.setEmpDetail(detail);
+//            detail.setEmployee(emp);
+//
+//            session.beginTransaction();
+//            session.save(detail);
+//            session.getTransaction().commit();
+//
+//            System.out.println("done!");
+
+            session = factory.getCurrentSession();
+
+            session.beginTransaction();
+            Detail detail = session.get(Detail.class, 1);
+            session.delete(detail);
+
+            session.getTransaction().commit();
+            System.out.println("done!");
+
+        }
+
+        finally {
+            session.close();
+            factory.close();
+        }
+    }
+}
